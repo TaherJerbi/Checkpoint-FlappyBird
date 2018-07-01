1b87764068889b45063a8755b117f526616df9fb
TaherJerbi <jerbi.taherr@gmail.com>
2018-07-01 17:34:44 +0200

    Minor changes

--- Checkpoint-FlappyBird/Assets/Scripts/CameraFollow.cs
+++ Checkpoint-FlappyBird/Assets/Scripts/CameraFollow.cs
@@ -10,6 +10,7 @@ public class CameraFollow : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+        //Only follow on the X axis
         transform.position = new Vector3(range.x + playerTransform.position.x, transform.position.y, range.z + playerTransform.position.z);
     }
 }
--- Checkpoint-FlappyBird/Assets/Scripts/ObstacleSpawner.cs
+++ Checkpoint-FlappyBird/Assets/Scripts/ObstacleSpawner.cs
@@ -1,6 +1,7 @@
 ﻿using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour {
+    //Public varibles are editable from the inspector
     public GameObject obstaclePrefab;
     public float minY;
     public float maxY;
@@ -9,12 +10,13 @@ public class ObstacleSpawner : MonoBehaviour {
     {
         if(col.tag == "Obstacle")
         {
+            //Choose random Y position
             float obstacleY = Random.Range(minY, maxY);
-
+            //Destroy passed Obstacle
             Destroy(col.gameObject);
-
+            //Choose a position for the new obstacle
             Vector3 spawnPosition = new Vector3(transform.position.x + distance, obstacleY, 0);
-
+            //Instantiate the new obstacle
             Instantiate(obstaclePrefab, spawnPosition , Quaternion.identity);
             
         }
